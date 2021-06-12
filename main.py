c0fb288d91f2977db9bcbf10da38401436a0659b
Jakub Kulewicz <jakubkulewicz05@gmail.com>
2021-06-12 10:40:38 +0200

    better histogram

--- main.py
+++ main.py
@@ -7,7 +7,11 @@ data_serviceTimes = pd.read_csv("data/Service_times.txt", sep=" ")
 data_serviceTimes.columns = ["Connection time"]
 data_callIntensity = pd.read_csv("data/Call_intensity.txt", sep="\t", skip_blank_lines=True)
 data_callIntensity.columns = ["Time_unit", "Number_of_submissions"]
+sorted_data_serviceTimes = data_serviceTimes.sort_values(by=['Connection time'], ascending = True)
+sorted_data_serviceTimes.insert(0, "ID", range(1, len(sorted_data_serviceTimes)+1))
+#sorted_data_serviceTimes = sorted_data_serviceTimes.reset_index()
 
+print(sorted_data_serviceTimes)
 
 # Calculate average service time
 avgServiceTime = data_serviceTimes.sum()/len(data_serviceTimes)
@@ -28,13 +32,17 @@ plt.xlim(0, 24)
 plt.ylim(0, 0.35)
 plt.grid(True)
 
+
+
 plt.subplot(122)
 #hist = avgTrafficIntensity["Number_of_submissions"].hist(bins=12)
-plt.hist(avgTrafficIntensity["Number_of_submissions"], density=1, facecolor='b', alpha=0.75, bins=12)
-plt.title("Histogram najczęściej występujących wartości natężeń")
-plt.xlabel("Natężenie ruchu A")
-plt.ylabel("Częstość występowania")
-plt.xlim(0, 0.35)
+plt.hist(sorted_data_serviceTimes["Connection time"], density=1, facecolor='b', alpha=0.75, bins = 50)
+#plt.plot(sorted_data_serviceTimes["ID"], sorted_data_serviceTimes["Connection time"], label = "Natężenie")
+plt.title("Histogram czasów połączeń")
+plt.xlabel("Czas połączenia")
+plt.ylabel("Prawdopodobieństwo")
+plt.xlim(0, 1000)
+plt.ylim(0, 0.0055)
 plt.grid(True)
 
 plt.show()
